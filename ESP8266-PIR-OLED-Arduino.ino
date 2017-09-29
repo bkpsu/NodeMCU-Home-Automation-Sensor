65acf72cf5e31cce32955b93308343df05a64a14
Bart K <bartus@gmail.com>
2017-09-28 21:55:31 -0400

    Added configuration options, cleaned up code
    -Added hardware config options/compiler directives for different sensor/hardware configs (OLED SPI/I2C, Motion sensor, Deep Sleep, ...).
    -Cleaned up code to show all config options up top
    -Added Deep Sleep and Motion Sensor functionality (motion sensor shows "M" in top right corner of display if fired off).

--- ESP8266-PIR-OLED-Arduino.ino
+++ ESP8266-PIR-OLED-Arduino.ino
@@ -2,47 +2,64 @@
 
 #define MQTT_SOCKET_TIMEOUT 120
 
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
-#include "DHT.h"
-#include "SSD1306Spi.h" //OLED Lib for SPI version
-//#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
-//#include "SSD1306.h" //OLED Lib for I2C version
-
+/************ HARDWARE CONFIG (CHANGE THESE FOR YOUR SENSOR SETUP) ******************/
+//#define OLED_SPI //Uncomment if using SPI OLED screen (assuming I2C otherwise)
+//#define DEEPSLEEP //Uncomment if you want sensor to sleep after every update (Does NOT work with MOTION_ON or LED_ON which require constant uptime) - Note: Screen will turn off/on during reboot cycle
+#define FLIP_SCREEN //Uncomment if mounting to wall with USB connector on top
+#define MOTION_ON //Uncomment if using motion sensor
+//#define OLED_MOTION //Uncomment if you want screen to turn on only if motion is detected
+//#define LED_ON //Uncomment if using as LED controller
 
 /************ WIFI and MQTT INFORMATION (CHANGE THESE FOR YOUR SETUP) ******************/
 #define wifi_ssid "ssid" //enter your WIFI SSID
 #define wifi_password "password" //enter your WIFI Password
-
-#define mqtt_server /*"192.168.2.229"*/ "openhabianpi" // Enter your MQTT server address or IP.
-#define mqtt_device "Ensuite" //MQTT device
+#define mqtt_server "mqtt_server" // Enter your MQTT server address or IP.
+#define mqtt_device "mqtt_device" //MQTT device
 #define mqtt_user "" //enter your MQTT username
 #define mqtt_password "" //enter your password
 
 /****************************** MQTT TOPICS (change these topics as you wish)  ***************************************/
-
-#define temperaturepub "home/Ensuite/temperature"
-#define humiditypub "home/Ensuite/humidity"
-#define tempindexpub "home/Ensuite/temperatureindex"
-#define motionpub "home/Ensuite/motion"
+#define temperaturepub "home/mqtt_device/temperature"
+#define humiditypub "home/mqtt_device/humidity"
+#define tempindexpub "home/mqtt_device/temperatureindex"
+#define motionpub "home/mqtt_device/motion"
 
 /*****************************************************/
 
-#define DHTPIN 4     // (D2) what digital pin we're connected to
-#define MOTIONPIN 5 // (D1) what digital pin the motion sensor is connected to
+#define DHTPIN 4      // (D2) what digital pin we're connected to
+#define MOTIONPIN 5   // (D1) what digital pin the motion sensor is connected to
 
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "DHT.h"
+
+#ifdef OLED_SPI
+  #include "SSD1306Spi.h" //OLED Lib for SPI version
+#else 
+  #include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+  #include "SSD1306.h" //OLED Lib for I2C version
+#endif
+
 DHT dht(DHTPIN, DHTTYPE);
 
-long previousMillis = 0;
-long interval = 60000;
+unsigned long currentMillis = 60001; //Set so temperature's read on first scan of program
+unsigned long previousMillis = 0;
+unsigned long interval = 60000;
+
+float h,t,f;
+int motionState;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-SSD1306Spi        display(D10, D9, D8); //RES, DC, CS (SPI Initialization)
-//SSD1306  display(0x7a, D7, D5); //SDA, SCL (I2C Initialization)
+#ifdef OLED_SPI
+  SSD1306Spi  display(D10, D9, D8); //RES, DC, CS (SPI Initialization)
+#else
+  SSD1306     display(0x3c, 3 /*D9*/, 1 /*D10*/); //0 /*D3*/, 2 /*D4*/); //SDA, SCL (I2C Initialization)
+#endif
+
 
 void setup() {
   Serial.begin(9600);
@@ -54,15 +71,16 @@ void setup() {
 
   display.init();
 
-  //display.flipScreenVertically(); //Uncomment if mounting to wall with USB connector on top
-  //display.invertDisplay(); //uncomment to use display horizontally (TODO: make x,y offsets relative)
+  #ifdef FLIP_SCREEN
+    display.flipScreenVertically(); 
+  #endif
   display.setFont(ArialMT_Plain_10);
   display.setTextAlignment(TEXT_ALIGN_LEFT);
     
   setup_wifi();
 
   client.setServer(mqtt_server, 1883); //CHANGE PORT HERE IF NEEDED
-  
+
 }
 
 void setup_wifi() {
@@ -72,12 +90,12 @@ void setup_wifi() {
   Serial.print("Connecting to ");
   Serial.println(wifi_ssid);
 
-  display.drawString(0,0,"Wifi: " + String(wifi_ssid));
-  display.display();
+  //display.drawString(0,0,"Wifi: " + String(wifi_ssid));
+  //display.display();
   
   WiFi.mode(WIFI_STA);
   WiFi.begin(wifi_ssid, wifi_password);
-
+  //WiFi.printDiag(Serial);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -88,26 +106,15 @@ void setup_wifi() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  display.setTextAlignment(TEXT_ALIGN_RIGHT);
-  display.drawString(120,10, "OK");
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(0,20, "IP: " + WiFi.localIP());
-  display.display();
+  //display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  //display.drawString(120,10, "OK");
+  //display.setTextAlignment(TEXT_ALIGN_LEFT);
+  //display.drawString(0,20, "IP: " + WiFi.localIP());
+  //display.display();
 }
 
-void drawDHT(float h, float t, float f){
- /* display.setFont(ArialMT_Plain_10);
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(0,0,"Humidity: ");
-  display.setTextAlignment(TEXT_ALIGN_RIGHT);
-  display.drawString(120,10, String(h) + " %");
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(0,20,"Temperature: ");
-  display.setTextAlignment(TEXT_ALIGN_RIGHT);
-  display.drawString(120,30, String (t) + " *C (" + String(f) + " *F)");
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(0,50, String(mqtt_device));*/
-
+void drawDHT(float h, float t, float f)
+{
   display.setFont(ArialMT_Plain_10);
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.drawString(0,0, String(mqtt_device));
@@ -119,7 +126,6 @@ void drawDHT(float h, float t, float f){
   display.drawString(0,50,"Hum: ");
   display.setTextAlignment(TEXT_ALIGN_RIGHT);
   display.drawString(120,50, String(h) + " %");
-  
 }
 
 void loop() {
@@ -131,26 +137,15 @@ void loop() {
   reconnect();
  }
  
-  int motionState = digitalRead(MOTIONPIN);
-  // print out the state of the button:
-  if (motionState == 1)
-  {
-    Serial.println("Motion Event:");
-    client.publish(motionpub, "1");
-    client.loop();
-    delay (4000);
-  }
-
-  unsigned long currentMillis = millis();
  
   if(currentMillis - previousMillis > interval) {
       previousMillis = currentMillis;
   
-      float h = dht.readHumidity();
+      h = dht.readHumidity();
       // Read temperature as Celsius (the default)
-      float t = dht.readTemperature();
+      t = dht.readTemperature();
       // Read temperature as Fahrenheit (isFahrenheit = true)
-      float f = dht.readTemperature(true);
+      f = dht.readTemperature(true);
     
       // Check if any reads failed and exit early (to try again).
       if (isnan(h) || isnan(t) || isnan(f)) {
@@ -190,11 +185,36 @@ void loop() {
       Serial.print(hif);
       Serial.println(" *F");
 
+      display.invertDisplay();
+      display.display();
       display.clear();
+      display.normalDisplay();
       drawDHT(h,t,f);
       display.display();
   }
+  currentMillis = millis();
 
+#ifdef MOTION_ON
+  motionState = digitalRead(MOTIONPIN);
+//   print out the state of the button:
+  if (motionState == 1)
+{
+    Serial.println("Motion Event:");
+    client.publish(motionpub, "1");
+    display.setTextAlignment(TEXT_ALIGN_RIGHT);
+    display.drawString(120,0, "M");
+    display.display();
+    client.loop();
+    delay (4000);
+    display.clear();
+    drawDHT(h,t,f);
+    display.display();
+  }
+#else
+  #ifdef DEEP_SLEEP
+   ESP.deepSleep(60000000,WAKE_RF_DEFAULT);   //If a motion sensor is not being used, we can put ESP into deep-sleep to save energy/wifi channel
+  #endif
+#endif
 }
 
 
@@ -202,14 +222,15 @@ void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    display.drawString(0,30, "MQTT: ");
-    display.display();
+    //display.drawString(0,30, "MQTT: ");
+    //display.display();
+
     // Attempt to connect
   if (client.connect(mqtt_device, mqtt_user, mqtt_password)) {
       Serial.println("connected");
-      display.setTextAlignment(TEXT_ALIGN_RIGHT);
-      display.drawString(120,40, "OK");
-      display.display();
+      //display.setTextAlignment(TEXT_ALIGN_RIGHT);
+      //display.drawString(120,40, "OK");
+      //display.display();
 
       client.subscribe(temperaturepub);
       client.subscribe(humiditypub);
